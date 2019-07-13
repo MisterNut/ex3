a9c2e1e033caca15bd3249e028e10c68dbf01297
Misternut Nuntes <59143459@g.cmru.ac.th>
2019-07-13 15:39:47 +0700

    Hellow git

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'โครต งง ' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'โครต งง ' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'โครต งง ' });
+});
+module.exports = router;
